9c4c1e493a4559e70f3ec8cccbaec1ec7831ef52
Simone Karani <simone.karani@gmail.com>
2020-10-25 15:45:45 -0700

    First commit for HS journal

---
+++ myHSJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myHSJournal
+//
+//  Created by Vijay Karani on 10/25/20.
+//  Copyright © 2020 simonekarani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
